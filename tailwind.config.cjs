cf668e84150a2eb30caf6e4a2356a9df45d5e06f
mozammilkarim <mozammilkarim@gmail.com>
2023-01-26 15:37:13 +0530

    layout complete without api

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily:{
+      'sans': ['ui-sans-serif', 'system-ui'],
+      'serif': ['ui-serif', 'Georgia'],
+      'mono': ['ui-monospace', 'SFMono-Regular'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
